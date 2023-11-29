836976ba579cea383bc1253c488f572d466ad5c0
zh15933 <32957590+zh15933@users.noreply.github.com>
2023-11-29 19:04:10 +0800

    更新 diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,6 +4,11 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+# 删除水哥库的passwall
+rm -rf openwrt/feeds/passwall1.*
+rm -rf openwrt/feeds/passwall2.*
+rm -rf openwrt/feeds/passwall3.*
+
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.5.1"            # 修改openwrt后台地址(填0为关闭)
