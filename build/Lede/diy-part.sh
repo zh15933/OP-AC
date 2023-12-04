8912a5f533e58e91e13039b5a24dbfe1edeb7f3d
zh15933 <32957590+zh15933@users.noreply.github.com>
2023-12-04 20:14:29 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -9,6 +9,11 @@ rm -rf feeds/passwall2
 rm -rf feeds/passwall1
 rm -rf feeds/helloworld
 
+
+git clone https://github.com/kenzok8/small.git package/lean/small
+
+
+
 # 后台IP设置
 export Ipv4_ipaddr="192.168.5.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
@@ -93,7 +98,7 @@ sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
 sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 sed -i 's/"设置向导"/"向导"/g' `egrep "设置向导" -rl ./`
 
-git clone https://github.com/kenzok8/small.git package/lean/small -f
+
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
