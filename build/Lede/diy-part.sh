ddbe8320ff04aef29428ec61c5771a76ab0ffaae
zh15933 <32957590+zh15933@users.noreply.github.com>
2023-12-04 20:11:50 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,8 +4,10 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-
-
+rm -rf feeds/passwall3
+rm -rf feeds/passwall2
+rm -rf feeds/passwall1
+rm -rf feeds/helloworld
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.5.1"            # 修改openwrt后台地址(填0为关闭)
