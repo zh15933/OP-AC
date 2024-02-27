3c9ad12e999c46d0b5c17bd273f34f0ca097fd59
zh15933 <32957590+zh15933@users.noreply.github.com>
2024-02-27 17:58:01 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -90,6 +90,26 @@ sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
+echo '替换golang到1.22.x'
+rm -rf feeds/packages/lang/golang
+git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+echo '=========Replace golang OK!========='
+
+echo '替换Passwall软件'
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
+mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
+echo '=========Replace passwall source OK!========='
+
+echo '修改Passwall检测规则'
+sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
+echo '=========ALTER passwall denpendcies check OK!========='
+
+echo '开启sing-box的CGO标记'
+sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/passwall/sing-box/Makefile
+echo '=========Enable sing-box CGO FLAG OK !========='
+
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
 packages
